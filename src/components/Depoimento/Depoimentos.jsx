f3dc0058c965241f360eb1defa01137fb9f1b264
João Vitor <jv86679@gmail.com>
2023-02-06 21:43:08 -0300

    ajustes depoimentos

--- src/components/Depoimento/Depoimentos.jsx
+++ src/components/Depoimento/Depoimentos.jsx
@@ -2,11 +2,10 @@ import React, { useState } from 'react'
 import './Depoimentos.scss'
 
 const depoimentos = [
-    {"Nome": "João Vitor", "Foto": "jovi", "Curso": "Ciência da Computação", "Universidade": "Unicamp"},
-    {"Nome": "Barbara Cristina", "Foto": "barbara", "Curso": "Medicina", "Universidade": "USP"},
-    {"Nome": "Marcia Anacleto", "Foto": "marcia", "Curso": "Pedagogia", "Universidade": "Unicamp"},
-    {"Nome": "Samuel Augusto", "Foto": "samuca", "Curso": "Geologia", "Universidade": "Unicamp"},
-    {"Nome": "Giovana Helena", "Foto": "giovana", "Curso": "Tec Mecatrônica", "Universidade": "Cotuca"},
+    {"Nome": "João Vitor", "Foto": "jovi", "Curso": "Ciência da Computação", "Universidade": "Unicamp", "Depoimento": "Eu sempre tive dificuldades com matemática e física, mas o cursinho me ajudou a compreender essas matérias de uma maneira mais clara e fácil. Agora eu posso dizer que me sinto confiante para enfrentar qualquer desafio na faculdade."},
+    {"Nome": "Barbara Cristina", "Foto": "barbara", "Curso": "Medicina", "Universidade": "USP", "Depoimento": "Antes de frequentar o cursinho, eu tinha dúvidas sobre qual carreira escolher. Com a ajuda dos professores e dos meus colegas de turma, pude descobrir qual área eu realmente tinha afinidade e interesse. Hoje, estou muito feliz e realizada com minha escolha."},
+    {"Nome": "Silas Eduardo", "Foto": "marcia", "Curso": "Ciências Sociais", "Universidade": "Unicamp", "Depoimento": "O cursinho me deu a oportunidade de me preparar para o vestibular e me destacar entre os outros candidatos. Além disso, fiz muitas amizades e aprendi muito com as aulas e os debates em grupo."},
+    {"Nome": "Samuel Augusto", "Foto": "samuca", "Curso": "Geologia", "Universidade": "Unicamp", "Depoimento": "Eu sempre tive dificuldades para me expressar e me comunicar de forma clara e efetiva. Com as aulas de redação e oratória do cursinho, eu consegui superar esse medo e hoje sou capaz de me expressar de forma confiante e coerente em qualquer situação."},
 ]
 
 function Depoimentos() {
@@ -42,7 +41,7 @@ function Depoimentos() {
             </div>
 
             <div className='depoimento-texto'>
-                "Conheço e participo do Herbert desde que me entendo como pessoa, as experiências, vivências, aprendizados e o tempo no projeto foram marcos que dividiram minha vida, sou grato a todos que mantém esse projeto vivo!"
+                "{depoimentos[counter["Depoimento"]]}"
             </div>
 
             <div className='depoimentos-counter'>
